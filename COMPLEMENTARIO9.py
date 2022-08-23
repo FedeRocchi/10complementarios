3fbcd74381064d186a07d7c080cc71e98f038e09
FedeRocchi <110907542+FedeRocchi@users.noreply.github.com>
2022-08-23 14:24:48 -0300

    PRIMEROS 10 COMPLEMENTARIOS SIN IF
    3ra carpeta, en está están los primeros complementarios sin if

---
+++ COMPLEMENTARIO9.py
@@ -0,0 +1,14 @@
+print("--------------------------------")
+print("TP COMPLEM 9")
+print("---------------------------------")
+
+PI = 3.14
+
+print("Ingrese ángulo en radianes")
+x = float(input())
+
+sex = x*180/pi
+cen = x200/pi
+
+print("En sexagesimales es :", sex)
+print("En centesimales es :", cen)
